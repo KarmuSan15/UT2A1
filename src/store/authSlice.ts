d5edfa7e6b6daa7defbc556acd1051e0e970010e
KarmuSan15 <toruhlahera@gmail.com>
2024-11-20 23:52:10 +0000

    Añadir funcionalidad de CRUD

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -1,4 +1,4 @@
-// Importamos la función createSlice de Redux Toolkit
+/*// Importamos la función createSlice de Redux Toolkit
 import { createSlice } from '@reduxjs/toolkit'
 
 // Definimos la estructura del estado de autenticación
@@ -39,4 +39,42 @@ const authSlice = createSlice({
 // Exportamos las acciones para usarlas en otros componentes
 export const authActions = authSlice.actions
 // Exportamos el reducer para integrarlo en el store
-export default authSlice.reducer
+export default authSlice.reducer*/
+
+
+//nuevo authslice para que funcione con la nueva CASA por la base de dato
+
+import { createSlice } from '@reduxjs/toolkit';
+
+interface AuthState {
+  nombreUsuario: string;
+  rol: string;
+  isAuthenticated: boolean;
+}
+
+const initialState: AuthState = {
+  nombreUsuario: '',
+  rol: '',
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: 'authenticator',
+  initialState,
+  reducers: {
+    login(state, action) {
+      state.nombreUsuario = action.payload.nombreUsuario;
+      state.rol = action.payload.rol;
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.nombreUsuario = '';
+      state.rol = '';
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+export default authSlice.reducer;
+
